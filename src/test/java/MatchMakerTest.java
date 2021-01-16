886b0803c64b3ccbae81ea2b524c85da05cfa8f7
alicjak1519 <akrzeminska1519@gmail.com>
2021-01-16 19:43:23 +0100

    test/Add test to buy-sell orders matchmaking

--- src/test/java/MatchMakerTest.java
+++ src/test/java/MatchMakerTest.java
@@ -1,5 +1,25 @@
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class MatchMakerTest {
 
+    @Test
+    public void matchMake() {
+        //given
+        var corporation = new Corporation(10, "Allegro");
+        var stockHolder = new Stockholder(20, Map.of(corporation, 20));
+        var buyOrders = List.of(new BuyOrder(corporation, stockHolder, 20, 1));
+        var sellOrders = List.of(new SellOrder(corporation, stockHolder, 20, 1));
+
+        //when
+        var stockOrdersPairs = new MatchMaker().matchMake(buyOrders, sellOrders);
+
+        //then
+        assertEquals(1, stockOrdersPairs.size());
+        assertEquals(stockOrdersPairs.get(0), new StockOrdersPair(sellOrders.get(0), buyOrders.get(0)));
+    }
 }
